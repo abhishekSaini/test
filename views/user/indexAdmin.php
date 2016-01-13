dae1b22ed6ff3234a48bcb32df3108f79ffcf951
admin <enuke-lapi-2@en.(none)>
2016-01-13 10:37:30 +0530

    fix

--- views/user/indexAdmin.php
+++ views/user/indexAdmin.php
@@ -25,7 +25,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             //['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+            //'id',
             //'password',
             'first_name',
             'last_name',
